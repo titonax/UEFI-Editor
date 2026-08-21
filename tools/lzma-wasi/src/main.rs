e9a16d548647fc1844242a04b069d3df7b9a8b78
titonax <74790560+titonax@users.noreply.github.com>
2026-08-22 01:07:08 +0200

    Support Tiano-compressed Aptio IV Setup sections

--- tools/lzma-wasi/src/main.rs
+++ tools/lzma-wasi/src/main.rs
@@ -1,16 +1,25 @@
 use std::env;
-use std::fs::File;
-use std::io::{BufReader, BufWriter};
+use std::fs;
+
+use efi_compress::{decompress, Compression};
 
 fn main() {
     let arguments: Vec<String> = env::args().collect();
-    if arguments.len() != 3 {
-        eprintln!("usage: lzma-decompress input.lzma output.bin");
+    if arguments.len() != 4 {
+        eprintln!("usage: firmware-decompress input.bin output.bin lzma|standard");
         std::process::exit(2);
     }
 
-    let input = File::open(&arguments[1]).expect("cannot open LZMA input");
-    let output = File::create(&arguments[2]).expect("cannot create decompressed output");
-    lzma_rs::lzma_decompress(&mut BufReader::new(input), &mut BufWriter::new(output))
-        .expect("LZMA decompression failed");
+    let input = fs::read(&arguments[1]).expect("cannot open compressed input");
+    let output = match arguments[3].as_str() {
+        "lzma" => decompress(&input, Compression::Lzma),
+        "standard" => decompress(&input, Compression::Tiano)
+            .or_else(|_| decompress(&input, Compression::EfiStandard)),
+        _ => {
+            eprintln!("unknown compression mode");
+            std::process::exit(2);
+        }
+    }
+    .expect("firmware decompression failed");
+    fs::write(&arguments[2], output).expect("cannot write decompressed output");
 }
